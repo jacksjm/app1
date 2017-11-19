a43c8b39d68cbcd01319fe4f6b0ea5c3e9c33fc7
Jackson Machado <jackson_machado_12@hotmail.com>
2017-11-19 17:52:47 -0200

    Liberação inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { PainelComponent } from './painel/painel.component';
+import { TentativaComponent } from './tentativa/tentativa.component';
+import { ProgressoComponent } from './progresso/progresso.component';
+
+@NgModule({
+  declarations: [
+	AppComponent,
+	TopoComponent,
+	PainelComponent,
+	TentativaComponent,
+	ProgressoComponent
+  ],
+  imports: [
+	BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
